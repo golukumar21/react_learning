9cf86af7c2c3c10d9a71f16ede16d58bd249514d
Kumar Abhishek <golukumar21@gmail.com>
2019-06-14 17:34:02 +0530

    separate class for header and contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,63 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            data: 
+            [
+                {
+                    "id": 1,
+                    "name": "golu",
+                    "age": 25
+                },
+                {
+                    "id": 2,
+                    "name": "chotu",
+                    "age": 24
+                },
+                {
+                    "id": 3,
+                    "name": "giddu",
+                    "age": 3
+                }
+            ]
+        }
+    }
     render() {
         return (
             <div>
-                <h1>
-                    This is APP component of React.
-                </h1>
+                <Header />
+                <table>
+                    <tbody>
+                        {this.state.data.map((person, i) => <TableRow key = {i}
+                            data ={ person } />)}
+                    </tbody>
+                </table>
             </div>
+        )
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Header</h1>
+            </div>
+        )
+    }
+}
+
+class TableRow extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td><strong>{this.props.data.id}</strong></td>
+                <td><strong>{this.props.data.name}</strong></td>
+                <td><strong>{this.props.data.age}</strong></td>
+            </tr>
         )
     }
 }
