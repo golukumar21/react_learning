20776d4d2216d8b4e8331fcda897a47ba1e0353d
Kumar Abhishek <golukumar21@gmail.com>
2019-06-14 18:12:33 +0530

    header and content using state and props

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,49 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            data: 
-            [
-                {
-                    "id": 1,
-                    "name": "golu",
-                    "age": 25
-                },
-                {
-                    "id": 2,
-                    "name": "chotu",
-                    "age": 24
-                },
-                {
-                    "id": 3,
-                    "name": "giddu",
-                    "age": 3
-                }
-            ]
+            header : "Header from props....",
+            content : "Content from props...."
         }
+        // this.state = {
+        //     data: 
+        //     [
+        //         {
+        //             "id": 1,
+        //             "name": "golu",
+        //             "age": 25
+        //         },
+        //         {
+        //             "id": 2,
+        //             "name": "chotu",
+        //             "age": 24
+        //         },
+        //         {
+        //             "id": 3,
+        //             "name": "giddu",
+        //             "age": 3
+        //         }
+        //     ]
+        //     header: "Header from state...",
+        //     content: "Content from state..."
+        // }
     }
     render() {
         return (
             <div>
-                <Header />
+                {/* <Header />
                 <table>
                     <tbody>
                         {this.state.data.map((person, i) => <TableRow key = {i}
                             data ={ person } />)}
                     </tbody>
-                </table>
+                </table> */}
+                {/* <h1>{this.props.headerProps}</h1>
+                <h2>{this.props.contentProps}</h2> */}
+                <Header headerProp={this.state.header}/>
+                <Content contentProp={this.state.content} />
             </div>
         )
     }
@@ -44,21 +54,36 @@ class Header extends React.Component {
     render() {
         return (
             <div>
-                <h1>Header</h1>
+                <h1>{this.props.headerProp}</h1>
             </div>
         )
     }
 }
 
-class TableRow extends React.Component {
+class Content extends React.Component {
     render() {
-        return (
-            <tr>
-                <td><strong>{this.props.data.id}</strong></td>
-                <td><strong>{this.props.data.name}</strong></td>
-                <td><strong>{this.props.data.age}</strong></td>
-            </tr>
+        return(
+            <div>
+                <h2>{this.props.contentProp}</h2>
+            </div>
         )
     }
 }
+// class TableRow extends React.Component {
+//     render() {
+//         return (
+//             <tr>
+//                 <td><strong>{this.props.data.id}</strong></td>
+//                 <td><strong>{this.props.data.name}</strong></td>
+//                 <td><strong>{this.props.data.age}</strong></td>
+//             </tr>
+//         )
+//     }
+// }
+
+// App.defaultProps = {
+//     headerProps : "Header from props....",
+//     contentProps : "Content from props...."
+// }
+
 export default App;
